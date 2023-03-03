f2c4fc9e8645b46fa5910437daaf23df9fdf65a1
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-03 20:30:20 +0000

    typescript setup and debugging

---
+++ hello-world/src/index.ts
@@ -0,0 +1,18 @@
+// console.log('Hello World!')
+
+// let age: number = 20
+// if (age > 50) {
+//   age += 10
+
+// console.log(age)
+
+let sales = 123_456_789
+let course = 'TypeScript'
+let isPublished = true
+
+let level
+level = 1
+level = 'a'
+
+// }
+
